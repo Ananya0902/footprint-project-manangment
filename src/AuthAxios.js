7ef397b5ffadb25fa1069c025d963b657ce1027d
= <=>
2024-02-03 17:25:29 +0530

    Everything is now in env variables

--- src/AuthAxios.js
+++ src/AuthAxios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const authAxios = axios.create()
+const authAxios = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL
+})
 
 export const setAuthToken = (token) => {
   if (token) {
