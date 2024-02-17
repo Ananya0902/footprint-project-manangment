eb77b7f4d7181ea25840e0bff9e4b680ade671b7
Arnab <iamarnabbiswas2001@gmail.com>
2024-02-18 01:02:20 +0530

    New

--- src/components/Auth/ForgetPassword.jsx
+++ src/components/Auth/ForgetPassword.jsx
@@ -8,205 +8,103 @@ import {
   Button,
   Select,
   FormErrorMessage,
-  useToast
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 export default function ForgetPassword() {
-    const [selectedValue, setSelectedValue] = useState("applicant");
-    const [loading,setLoading]=useState(false)
-    const [email, setEmail] = useState("");
-    const toast=useToast();
-    const [errors, setErrors] = useState({
-        email: "",
+  const [selectedValue, setSelectedValue] = useState("applicant");
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const toast = useToast();
+  const [errors, setErrors] = useState({
+    email: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+    setErrors({
+      ...errors,
+      email: "",
+    });
+  };
+  const submitHandler = async () => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+    const newErrors = {};
+
+    if (emailRegex.test(email) === false) {
+      newErrors.email = "Enter a valid email";
+    }
+
+    setErrors(newErrors);
+    if (Object.keys(newErrors).length > 0) {
+      toast({
+        title: "please Enter valid credentials",
+        description: "some fiels are not valid",
+        position: "top",
+        status: "error",
+        duration: 1000,
+        isClosable: true,
       });
-      const navigate=useNavigate();
-
-    const handleChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
-    const emailChangeHandler = (event) => {
-        setEmail(event.target.value);
-        setErrors({
-          ...errors,
-          email: "",
-        });
-      };
-      const submitHandler = async () => {
-
-        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-
-        const newErrors = {};
-       
-    
-       
-        if (emailRegex.test(email) === false) {
-          newErrors.email = "Enter a valid email";
+    } else {
+      setLoading(true);
+      const result = await fetch(
+        `http://localhost:5000/api/v1/users/sendemail${selectedValue}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+          }),
         }
-    
-      
-        setErrors(newErrors);
-        if (Object.keys(newErrors).length > 0) {
+      );
+      const data = await result.json();
+      setLoading(false);
+      // console.log(data);
+      if (data.success === false) {
+        if (data.msg === "User not exists") {
           toast({
-            title: "please Enter valid credentials",
-            description: "some fiels are not valid",
+            title: "This Email doesn't exists",
+            description: "Resgister first",
             position: "top",
             status: "error",
             duration: 1000,
             isClosable: true,
           });
-        } else{
-            setLoading(true);
-            const result=await fetch(`http://localhost:5000/api/v1/users/sendemail${selectedValue}`,{
-                method:"POST",
-                headers:{
-                    "Content-Type": "application/json",
-                },
-                body:JSON.stringify({
-                    email:email
-                })
-            })
-            const data= await result.json();
-            setLoading(false)
-            // console.log(data);
-            if(data.success===false){
-                if(data.msg==='User not exists'){
-                    toast({
-                        title: "This Email doesn't exists",
-                        description: "Resgister first",
-                        position: "top",
-                        status: "error",
-                        duration: 1000,
-                        isClosable: true,
-                      });
-                }
-                else{
-                    toast({
-                        title: "Something went wrong",
-                        description: "Try after some time",
-                        position: "top",
-                        status: "error",
-                        duration: 1000,
-                        isClosable: true,
-                      });
-                }
-            }
-            else{
-                toast({
-                    title: "OTP sent successfully",
-                    position: "top",
-                    status: "success",
-                    duration: 1000,
-                    isClosable: true,
-                  });
-                  setLoading(false);
-                  localStorage.setItem('token',data.token)
-                  setTimeout(()=>{
-                        navigate('/emailotp')
-                  },1500)
-            }
-
+        } else {
+          toast({
+            title: "Something went wrong",
+            description: "Try after some time",
+            position: "top",
+            status: "error",
+            duration: 1000,
+            isClosable: true,
+          });
         }
-
-
-
-        // else {
-        //   setLoading(true);
-        //   const result = await fetch("http://localhost:5000/api/v1/users/signup", {
-        //     method: "POST",
-        //     headers: {
-        //       "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //       firstname: fname,
-        //       lastname: lname,
-        //       mobile: mobile,
-        //       email: email,
-        //     }),
-        //   });
-        //   const data = await result.json();
-        //   if (data.success === true) {
-        //     localStorage.setItem("token", data.token);
-        //     if (
-        //       data.message === "Email and Mobile is not varified" ||
-        //       data.message === "Mobile not varified" ||
-        //       data.message === "User created"
-        //     ) {
-        //       const response = await fetch(
-        //         "http://localhost:5000/api/v1/users/sendmobile",
-        //         {
-        //           method: "POST",
-        //           headers: {
-        //             "Content-Type": "application/json",
-        //             Authorization: `Bearer ${data.token}`,
-        //           },
-        //         }
-        //       );
-        //       const res = await response.json();
-        //       if (res.success === true) {
-        //         navigate("/mobile");
-        //       } else {
-        //         setLoading(false);
-        //         toast({
-        //           title: "Some Error Happened",
-        //           description: "Try after some time",
-        //           position: "top",
-        //           status: "error",
-        //           duration: 1000,
-        //           isClosable: true,
-        //         });
-        //       }
-        //     } else {
-        //       if (data.message === "Email not varified") {
-        //         const response = await fetch(
-        //           "http://localhost:5000/api/v1/users/sendemail",
-        //           {
-        //             method: "POST",
-        //             headers: {
-        //               "Content-Type": "application/json",
-        //               Authorization: `Bearer ${data.token}`,
-        //             },
-        //           }
-        //         );
-        //         const res = await response.json();
-        //         if (res.success === true) {
-        //           navigate("/email");
-        //         } else {
-        //           setLoading(false);
-        //           toast({
-        //             title: "Some Error Happened",
-        //             description: "Try after some time",
-        //             position: "top",
-        //             status: "error",
-        //             duration: 1000,
-        //             isClosable: true,
-        //           });
-        //         }
-        //       }
-        //     }
-        //   } else {
-        //     if (data.message === "Mobile and Email is varified") {
-        //       navigate("/welcome");
-        //     } else {
-        //       setLoading(false);
-        //       toast({
-        //         title: "Internal server error",
-        //         description: "Try after some time",
-        //         position: "top",
-        //         status: "error",
-        //         duration: 1000,
-        //         isClosable: true,
-        //       });
-        //     }
-        //   }
-        // }
-      };
-
-
-
-
+      } else {
+        toast({
+          title: "OTP sent successfully",
+          position: "top",
+          status: "success",
+          duration: 1000,
+          isClosable: true,
+        });
+        setLoading(false);
+        localStorage.setItem("token", data.token);
+        setTimeout(() => {
+          navigate("/emailotp");
+        }, 1500);
+      }
+    }
+  };
 
   return (
     <Center w={"100vw"} h={"100vh"} bgColor={"#FFDEAD"}>
@@ -227,17 +125,17 @@ export default function ForgetPassword() {
           </Text>
           <FormControl id="userType" isRequired>
             <FormLabel>User Type</FormLabel>
-            <Select borderColor={"black"} value={selectedValue} onChange={handleChange}>
+            <Select
+              borderColor={"black"}
+              value={selectedValue}
+              onChange={handleChange}
+            >
               <option value="applicant">Applicant</option>
               <option value="reviewer">Reviewer</option>
               <option value="approver">Approver</option>
             </Select>
           </FormControl>
-          <FormControl
-            id="Email"
-            isRequired
-            isInvalid={errors.email}
-          >
+          <FormControl id="Email" isRequired isInvalid={errors.email}>
             <FormLabel>Email</FormLabel>
             <Input
               placeholder="Enter your Email"
@@ -246,18 +144,18 @@ export default function ForgetPassword() {
               borderColor={"black"}
             />
             {errors.email ? (
-                <FormErrorMessage>{errors.email}</FormErrorMessage>
-              ) : (
-                ""
-              )}
+              <FormErrorMessage>{errors.email}</FormErrorMessage>
+            ) : (
+              ""
+            )}
           </FormControl>
 
           <Button
             colorScheme="blue"
             width="50%"
             style={{ marginTop: 15 }}
-              onClick={submitHandler}
-              isLoading={loading}
+            onClick={submitHandler}
+            isLoading={loading}
           >
             Submit
           </Button>
